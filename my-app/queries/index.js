a0d09c7e2606565243c752782d0a9a9cccc5055e
SarathiNanavati <sarathi.nanavati@gmail.com>
2022-08-30 19:33:28 +0530

    Junior Chainlink VRF + GRAPH project completed

---
+++ my-app/queries/index.js
@@ -0,0 +1,22 @@
+export function FETCH_CREATED_GAME() {
+   return `query {
+        games(orderBy:id, orderDirection:desc,first:1){
+            id
+            maxPlayers
+            entryFee
+            winner
+            players
+        }
+    }`;
+}
+export function FETCH_ALL_GAME() {
+   return `query {
+        games(orderBy:id, orderDirection:desc){
+            id
+            maxPlayers
+            entryFee
+            winner
+            players
+        }
+    }`;
+}
